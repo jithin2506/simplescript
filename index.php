106947ff2210271f46ab7a3fdecaa74bbf9ba361
jithin2506 <56962141+jithin2506@users.noreply.github.com>
2019-10-24 16:56:42 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<center>
+<audio autoplay> <source src="http://www.sibirgorod.ru/again.mp3" type="audio/mpeg"> </audio>
+<head>
+<title>Pwned..??</title>
+<META NAME="description" CONTENT="Pwned by Monkey B Luffy"> 
+<META NAME="keywords" CONTENT="Monkey B Luffy"> 
+<META NAME="robot" CONTENT="index,follow"> 
+<META NAME="copyright" CONTENT="Copyright. All Rights Reserved."> 
+<META NAME="author" CONTENT="Pwned by Monkey B Luffy"> 
+<META NAME="language" CONTENT="English"> 
+<META NAME="revisit-after" CONTENT="1">
+<link href="http://cdn.wallpapersafari.com/31/90/JO6HyU.png" rel="SHORTCUT ICON"/>
+</head>
+<style type="text/css">
+html:before {
+  content: "";
+  position: fixed;
+  z-index: -1;
+  background-size:cover;
+  background-position:center top;
+  display: block;
+  background-image: url('https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/17796308_177870622730483_7463376706652682987_n.jpg?oh=3bf1f66b89558023dec0cbd5445cfa81&oe=594DB979');
+  width: 100%;
+  height: 100%;
+  filter: blur(7px) ;
+  -webkit-filter: blur(7px) ;
+}
+#title {
+  text-align:center;
+  color: lime; 
+ }
+#kaki {
+  background: transparent;
+  position:absolute;
+  bottom:0;
+  width:100%;
+  text-align:center;
+  color:lime;
+}
+i { 
+  color: lime;
+}
+a {
+color : silver;
+text-decoration: none;
+}
+</style>
+<div id="title">
+<center>
+<h1>Muweheheh by Monkey B Luffy</h1>
+<img src="http://cdn.wallpapersafari.com/31/90/JO6HyU.png" height="300" width="300">
+<h2>BerdendangC0de</h2>
+<font color="lime">We Are :</font><br>
+<font color="lime">./Mr.greetz69 - Kazuya404 - Jilan404 - ./N4sKun - Manusia Berdendang - Effect - B0c4H_Id30T - root@ashura - Fuss - Sector.V2</font><br>
+</center>
+<br><br>
+</div>
+<?php 
+echo "
+<div id='kaki'>
+<hr><br>
+<font> Copyright &copy; 2017. Powered By <a href='https://bc0de.net'>BC0DE.NET</a> & <a href='https://naonlah.net'> NAONLAH.NET<a>
+</font>
+</div>";
+?>
+</html>
